84c0663f32702d1fb9960a4d9e9d8c6cf4a275ad
Mirko Robbi <mirko.robbi@gmail.com>
2022-10-28 16:52:25 +0200

    finish exercise and bonus

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 // Descrizione:
 // Attraverso l'apposita API di Boolean
 // https://flynn.boolean.careers/exercises/api/random/mail
-// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.Bonus
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+//Bonus
 // Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 
 const { createApp } = Vue;
@@ -9,20 +10,41 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            numbersArray: [],
+            itemsEmail: [],
+            interval: "",
+            cont: 10,
+            emailsArray: [],
         };
     },
     created() {
-        this.getEmails();
     },
+
     methods: {
         getEmails() {
             axios
                 .get("https://flynn.boolean.careers/exercises/api/random/mail")
                 .then((resp) => {
-                    console.log(resp.data.response);
                     // this.rndNumber = resp.data.response;
+                    this.itemsEmail.push(resp.data.response)
+                    console.log(this.itemsEmail);
                 });
+        },
+        buttonGenerate() {
+            if (this.itemsEmail.length < this.cont) {
+                for (let i = 0; i < this.cont; i++) {
+                    this.getEmails()
+                }
+                console.log(this.itemsEmail, "array");
+            }
+
+            // this.interval = setInterval(() => {
+            //     this.emailsArray.push(this.itemEmail)
+            //     clearInterval(this.interval)
+            // }, 500);
+
+            // console.log(this.itemEmail, "item");
+            // console.log(this.emailsArray, "array");
+
         },
     },
 }).mount("#app");
